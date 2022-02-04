dda9d8f0329feb72f3a8ccd235149edf87bbc6bc
Wouter Hilderson <wouter.hilderson@gmail.com>
2022-02-04 17:12:55 +0100

    dashboard see pending reservations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @user = current_user
+    @name = current_user.name || "you incog'ito matey"
+    @pending_ships = current_user.bookings.select { |booking| booking.status == "pending" }.map(&:pirate_ship)
+    @confirmed_bookings = current_user.bookings.select { |booking| booking.status == "confirmed" }
+  end
+
 end
