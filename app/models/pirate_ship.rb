4522c1ed42d9fc38b9e4bad5181ba41027e25527
Marthe Mukamazimpaka <marthemukam@gmail.com>
2022-02-05 15:30:06 +0100

    validations

--- app/models/pirate_ship.rb
+++ app/models/pirate_ship.rb
@@ -3,4 +3,7 @@ class PirateShip < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   has_many_attached :pictures
+
+  validates :name, :description, presence: true, uniqueness: true
+
 end
