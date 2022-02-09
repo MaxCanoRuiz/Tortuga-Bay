a649bb6a54073e3d5c5b4fd4c973226f5bac1823
Max Cano <max.cano1999@gmail.com>
2022-02-09 13:20:01 +0100

    map functionality with markers added

--- app/models/pirate_ship.rb
+++ app/models/pirate_ship.rb
@@ -4,6 +4,9 @@ class PirateShip < ApplicationRecord
   has_many :users, through: :bookings
   has_many_attached :pictures
 
+  geocoded_by :port
+  after_validation :geocode, if: :will_save_change_to_port?
+
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
 
